965d5e64f422b255ff1b1edb177d3c839e947610
Mahesh Natarajan <maheshnatarajan@Maheshs-MacBook-Pro.local>
2026-07-30 15:16:00 -0400

    changes

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "hello from render"}), 200
+
+
+@app.route('/fetch_comment', methods=['GET'])
+def fetch_comment():
+    return jsonify({"message": "Fetch comments from google drive"}), 200
+
+
+@app.route('/post_comment', methods=['POST'])
+def post_comment():
+    return jsonify({"message": "Posted comments to google drive"}), 200
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
